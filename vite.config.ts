80b8b110eb205a20e494bc9725110ea10ddb1034
Zayar Tun <zayar.tun@brycenmyanmar.com.mm>
2025-02-14 09:54:01 +0630

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will_you_be_my_valentine",
+  base: "/will-you-be-my-valentine",
   plugins: [react()],
 });
